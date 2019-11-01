16d2cd631c3bc8ca0a2418623aef9fe3763f22ca
Lower Zhou <lowerzhou@LowerdeMBP.lan>
2019-11-01 22:42:23 +0800

    更新代码

---
+++ src/main/java/com/rest/RestAssured.java
@@ -0,0 +1,12 @@
+package com.rest;
+
+/**
+ * @author : xuanke
+ * @version V1.0
+ * @Email: zhouliwei1989@126.com
+ * @date Date : 2019年11月01日 10:41 下午
+ * @Description: rest-assured-wrapper
+ */
+
+public class RestAssured {
+}
